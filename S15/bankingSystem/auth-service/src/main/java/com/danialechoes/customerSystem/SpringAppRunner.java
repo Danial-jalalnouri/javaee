6781513fbb70d43a9e52648cd7df29c86da2b9cf
Danial <>
2025-08-11 19:59:23 +0200

    S15

---
+++ S15/bankingSystem/auth-service/src/main/java/com/danialechoes/customerSystem/SpringAppRunner.java
@@ -0,0 +1,22 @@
+package com.danialechoes.customerSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+import java.io.IOException;
+import java.util.Properties;
+
+@SpringBootApplication
+public class SpringAppRunner {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(SpringAppRunner.class);
+        app.run(args);
+    }
+
+}
---
+++ S15/bankingSystem/customer-service/src/test/java/com/danialechoes/customerSystem/SpringAppRunnerTests.java
@@ -0,0 +1,13 @@
+package com.danialechoes.customerSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppRunnerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
